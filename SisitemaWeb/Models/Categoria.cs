80f0e05c5482e0d323c9dce6bc22d605e9a0ed60
Pedro Henrique R. Santos <pe.phsants0@gmail.com>
2022-03-24 12:27:32 -0300

    parte 2

---
+++ SisitemaWeb/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SisitemaWeb.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Descricao { get; set; }
+    }
+}
---
+++ SisitemaWeb/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SisitemaWeb.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
